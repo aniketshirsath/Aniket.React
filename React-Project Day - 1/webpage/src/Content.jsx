a6388eb98d820b8d4bd3bc840744064eb14d9b01
Aniketshirsath1411 <aniketshirsath1411@gmail.com>
2024-06-19 14:16:20 +0530

    updated

--- React-Project Day - 1/webpage/src/Content.jsx
+++ React-Project Day - 1/webpage/src/Content.jsx
@@ -2,18 +2,58 @@ import React from 'react';
 
 function Content() {
   return (
-    <div className="container my-5">
-      <div className="row">
-        <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
-          <h1>Brand Salman Khan</h1>
-          <p style={{textAlign: 'justify'}}>
-            Founded by the fitness enthusiast Salman Khan himself, SK-27 strives to provide affordable gyms with extensive expertise and state-of the art equipment. With the highest fan following as the fitness icon, Salman Khan inspires a wide range of audience with more people opting to become healthier & fitter.
-          </p>
+    <div>
+      <section className="container my-5">
+        <div className="row">
+          <div className="col-12 col-md-6 d-flex flex-column justify-content-center">
+            <h1>Brand Salman Khan</h1>
+            <p style={{textAlign: 'justify'}}>
+              Founded by the fitness enthusiast Salman Khan himself, SK-27 strives to provide affordable gyms with extensive expertise and state-of the art equipment. With the highest fan following as the fitness icon, Salman Khan inspires a wide range of audience with more people opting to become healthier & fitter.
+            </p>
+          </div>
+          <div className="col-12 col-md-6">
+            <img src="https://www.beingstrong.in/images/the-brand.jpg" alt="Brand Salman Khan" className="img-fluid"/>
+          </div>
         </div>
-        <div className="col-12 col-md-6">
-          <img src="https://www.beingstrong.in/images/the-brand.jpg" alt="Brand Salman Khan" className="img-fluid"/>
+      </section>
+
+      <section id="services" className="py-5 bg-light">
+        <div className="container">
+          <div className="text-center mb-5">
+            <h2>Our Services</h2>
+            <p>We offer a range of services to meet your fitness needs</p>
+          </div>
+          <div className="row">
+            <div className="col-md-4 mb-4">
+              <div className="card h-100 shadow-sm">
+                <img src="https://activemanningham.com.au/wp-content/uploads/2024/02/No-branding-2024-02-28T091622.643-1024x683.jpg" className="card-img-top" alt="Service 1" />
+                <div className="card-body">
+                  <h5 className="card-title">Personal Training</h5>
+                  <p className="card-text">Get personalized workout plans and one-on-one training sessions with our certified trainers.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-4">
+              <div className="card h-100 shadow-sm">
+                <img src="https://cdn.create.vista.com/api/media/medium/153033740/stock-photo-sporty-people-exercising-in-gym?token=" className="card-img-top" alt="Service 2" />
+                <div className="card-body">
+                  <h5 className="card-title">Group Classes</h5>
+                  <p className="card-text">Join our group fitness classes, including yoga, pilates, Zumba, and more.</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4 mb-4">
+              <div className="card h-100 shadow-sm">
+                <img src="https://i0.wp.com/www.strengthlog.com/wp-content/uploads/2021/05/shutterstock_1621261183-1.jpg?fit=1000%2C666&ssl=1" className="card-img-top" alt="Service 3" />
+                <div className="card-body">
+                  <h5 className="card-title">Nutritional Guidance</h5>
+                  <p className="card-text">Receive expert advice on nutrition and diet plans to complement your fitness routine.</p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 }
--- React-Project Day - 1/webpage/src/Footer.jsx
+++ React-Project Day - 1/webpage/src/Footer.jsx
@@ -3,68 +3,60 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Footer() {
   return (
-    <footer className="bg-dark text-white text-center p-2">
-      <div className="container-fluid footer ">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-sm-12">
-              <h5>Mumbai Head Office</h5>
-              <ul className="list-unstyled">
-                <li>+91 75061 02102</li>
-                <li>+91 75061 05105</li>
-                <li>+91 75061 12233</li>
-                <li>+91 75061 10099</li>
-              </ul>
-            </div>
-            <div className="col-md-4 col-sm-12">
-              <h5>Other Locations</h5>
-              <div className="row">
-                <div className="col-6">
-                  <ul className="list-unstyled">
-                    <li>NAGPUR: 9322626565</li>
-                    <li>PUNE: 9322126161</li>
-                    <li>VADODARA: 9322126262</li>
-                    <li>DELHI: 9999011211</li>
-                    <li>BENGALURU: 7040050000</li>
-                    <li>BHUBANESWAR: 9320074456</li>
-                    <li>KOCHI: 9320011294</li>
-                    <li>JAIPUR: 9152727324</li>
-                    <li>HYDERABAD: 9321721010</li>
+    <footer className='bg-dark'>
+        <div class="container">
+            <div class="col-sm-12">
+              <div class="head-ftr">
+                <h2 className='text-center p-5 text-white'>Contact Us</h2>
+              </div>
+              <div class="row">
+                <div class="col-sm-3 pd0">
+                  <ul class="contct-div-nmbr">
+                    <li><a href="tel:+91 75061 02102"><span class="fa fa-chevron-right"></span>MUMBAI HEAD OFFICE : +91 75061 02102</a></li>
+                    <li><a href="tel:9322626565"><span class="fa fa-chevron-right"></span>NAGPUR : 9322626565 </a></li>
+                    <li><a href="tel:9322126161"><span class="fa fa-chevron-right"></span>PUNE : 9322126161</a></li>
+                    <li><a href="tel:9289012888/9289012889"><span class="fa fa-chevron-right"></span>GURUGRAM : 9289012888</a></li>
+
                   </ul>
                 </div>
-                <div className="col-6">
-                  <ul className="list-unstyled">
-                    <li>GOA: 9322126161</li>
-                    <li>GUWAHATI: 9322626565</li>
-                    <li>CHENNAI: 9322626565</li>
-                    <li>KOLKATA: 9820100695</li>
-                    <li>AHMEDABAD: 9322126262</li>
-                    <li>LUCKNOW: 9999011211</li>
-                    <li>INDORE: 9152727325</li>
-                    <li>CHANDIGARH: 9999011211</li>
-                    <li>GURUGRAM: 9289012888 / 9289012889</li>
+                <div class="col-sm-3 pd0">
+                  <ul class="contct-div-nmbr">
+                    <li><a href="tel:9322126262"><span class="fa fa-chevron-right"></span>VADODARA : 9322126262</a></li>
+                    <li><a href="tel:9999011211"><span class="fa fa-chevron-right"></span>DELHI : 9999011211</a></li>
+                    <li><a href="tel:7040050000"><span class="fa fa-chevron-right"></span>BENGALURU : 7040050000</a></li>
+                    <li><a href="tel:9320074456"><span class="fa fa-chevron-right"></span>BHUBANESWAR : 9320074456</a></li>
+                    <li><a href="tel:9320011294"><span class="fa fa-chevron-right"></span>KOCHI : 9320011294 </a></li>
+
+                  </ul>
+                </div>
+                <div class="col-sm-3 pd0">
+                  <ul class="contct-div-nmbr">
+                    <li><a href="tel:9152727324"><span class="fa fa-chevron-right"></span>JAIPUR : 9152727324</a></li>
+                    <li><a href="tel:9321721010"><span class="fa fa-chevron-right"></span>HYDERABAD : 9321721010</a></li>
+                    <li><a href="tel:9322126161"><span class="fa fa-chevron-right"></span>GOA : 9322126161</a></li>
+                    <li><a href="tel:9322626565"><span class="fa fa-chevron-right"></span>GUWAHATI : 9322626565 </a></li>
+                    <li><a href="tel:9322626565"><span class="fa fa-chevron-right"></span>CHENNAI : 9322626565</a></li>
+
+                  </ul>
+                </div>
+                <div class="col-sm-3 pd0">
+                  <ul class="contct-div-nmbr">
+                    <li><a href="tel:9820100695"><span class="fa fa-chevron-right"></span>KOLKATA : 9820100695</a></li>
+                    <li><a href="tel:9322126262"><span class="fa fa-chevron-right"></span>AHMEDABAD : 9322126262</a></li>
+                    <li><a href="tel:9999011211"><span class="fa fa-chevron-right"></span>LUCKNOW : 9999011211</a></li>
+                    <li><a href="tel:9152727325"><span class="fa fa-chevron-right"></span>INDORE : 9152727325</a></li>
+                    <li><a href="tel:9999011211"><span class="fa fa-chevron-right"></span>CHANDIGARH : 9999011211</a></li>
                   </ul>
                 </div>
               </div>
             </div>
-            <div className="col-md-4 col-sm-12">
-              <h5>Follow Us</h5>
-              <div className="social-icons">
-                <a href="#" className="text-white me-2"><i className="fab fa-facebook"></i></a>
-                <a href="#" className="text-white me-2"><i className="fab fa-youtube"></i></a>
-                <a href="#" className="text-white me-2"><i className="fab fa-instagram"></i></a>
-              </div>
-            </div>
-          </div>
-          <hr />
-          <div className="row mt-3">
-            <div className="col-12">
-              <p className="mt-2">&copy; 2020 SK-27 GYM All Rights Reserved. Developed By DESIGN ACCENT</p>
-              <span>Made By Aniket...</span>
-            </div>
-          </div>
         </div>
-      </div>
+        <div className='tagline text-white text-center p-5'>
+          <p>©2020 SK-27 GYM All Rights Reserved. Developed By <span className='text-danger'>DESIGN ACCENT</span></p>
+        </div>
+        <div className="social-icons">
+        
+        </div>
     </footer>
   );
 }
