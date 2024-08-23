1c41c63d85ed43a61e3a851db38b3b473c8a45e1
aniketshirsath <aniketshirsath1411@gmail.com>
2024-08-23 20:57:25 +0530

    update

--- Firebase Database/src/Components/Navbar.jsx
+++ Firebase Database/src/Components/Navbar.jsx
@@ -4,9 +4,8 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='d-flex justify-content-evenly fixed-top' style={{backgroundColor:'black',height:'60px',alignItems:'center'}}>
-      <Link to={"/"} style={{textDecoration:"none"}} className='fs-4 text-white fw-bold'>PRODUCTPAGE</Link>
-      <Link to={"/login"} style={{textDecoration:"none"}} className='fs-4 text-white fw-bold'>LOGIN</Link>
-      
+      <Link to={"/login"} style={{textDecoration:"none"}} className='fs-5 text-white fw-bold'>LOGIN</Link>
+      <Link to={"/"} style={{textDecoration:"none"}} className='fs-5 text-white fw-bold'>PRODUCTS</Link>
     </div>
   )
 }
